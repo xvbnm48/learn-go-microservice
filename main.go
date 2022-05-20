303c529bfc40420e7b9f159bf132407aea193fb4
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-05-20 22:22:28 +0700

    get by name, add item, edit item , delete item

--- main.go
+++ main.go
@@ -5,6 +5,52 @@ type Item struct {
 	body  string
 }
 
+var database []Item
+
+func GetByName(title string) Item {
+	var getItem Item
+
+	for _, val := range database {
+		if val.title == title {
+			getItem = val
+		}
+	}
+
+	return getItem
+}
+
+func AddItem(item Item) Item {
+	database = append(database, item)
+	return item
+}
+
+func EditItem(title string, edit Item) Item {
+	var changed Item
+
+	for idx, val := range database {
+		if val.title == title {
+			database[idx] = edit
+			changed = edit
+		}
+	}
+
+	return changed
+}
+
+func DeleteItem(item Item) Item {
+	var del Item
+
+	for idx, val := range database {
+		if val.title == item.title && val.body == item.body {
+			database = append(database[:idx], database[idx+1:]...)
+			del = item
+			break
+		}
+	}
+
+	return del
+}
+
 func main() {
 
 }
