0a38437d7edfad764fd6a27335b8019ea0e34963
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-05-21 22:44:51 +0700

    make client, refactoring main.go with function value from api

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
+import (
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+)
+
 type Item struct {
 	title string
 	body  string
 }
 
+type API int
+
 var database []Item
 
-func GetByName(title string) Item {
+func (a *API) GetByName(title string, reply *Item) error {
 	var getItem Item
 
 	for _, val := range database {
@@ -15,29 +24,30 @@ func GetByName(title string) Item {
 			getItem = val
 		}
 	}
-
-	return getItem
+	*reply = getItem
+	return nil
 }
 
-func AddItem(item Item) Item {
+func (a *API) AddItem(item Item, reply *Item) error {
 	database = append(database, item)
-	return item
+	*reply = item
+	return nil
 }
 
-func EditItem(title string, edit Item) Item {
+func (a *API) EditItem(edit Item, reply *Item) error {
 	var changed Item
 
 	for idx, val := range database {
-		if val.title == title {
-			database[idx] = edit
-			changed = edit
+		if val.title == edit.title {
+			database[idx] = Item{edit.title, edit.body}
+			changed = database[idx]
 		}
 	}
-
-	return changed
+	*reply = changed
+	return nil
 }
 
-func DeleteItem(item Item) Item {
+func (a *API) DeleteItem(item Item, reply *Item) error {
 	var del Item
 
 	for idx, val := range database {
@@ -47,10 +57,28 @@ func DeleteItem(item Item) Item {
 			break
 		}
 	}
+	*reply = del
+	return nil
 
-	return del
 }
 
 func main() {
+	var api = new(API)
+	err := rpc.Register(api)
 
+	if err != nil {
+		log.Fatal("Error registering API: ", err)
+	}
+
+	rpc.HandleHTTP()
+	listener, err := net.Listen("tcp", ":4040")
+	if err != nil {
+		log.Fatal("Error listening: ", err)
+	}
+
+	log.Printf("serving rpc on port %d", 4040)
+	err = http.Serve(listener, nil)
+	if err != nil {
+		log.Fatal("Error serving: ", err)
+	}
 }
